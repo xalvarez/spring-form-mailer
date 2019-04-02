84a29a569d07d86a54f9ff4b1d6503c3f1da4530
Xavier Alvarez <xalvarez@users.noreply.github.com>
2019-04-02 22:11:00 +0200

    Add index controller

---
+++ src/main/kotlin/com/github/xalvarez/springformmailer/IndexController.kt
@@ -0,0 +1,12 @@
+package com.github.xalvarez.springformmailer
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class IndexController {
+
+    @GetMapping
+    fun index() = "index"
+
+}
