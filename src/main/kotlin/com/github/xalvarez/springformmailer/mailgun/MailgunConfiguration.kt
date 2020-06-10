feddaa1356822e75f2e9da349088c92c26697afb
Xavier Alvarez <xalvarez@users.noreply.github.com>
2020-06-10 09:33:32 +0200

    Simplify MailGun Configuration

--- src/main/kotlin/com/github/xalvarez/springformmailer/mailgun/MailgunConfiguration.kt
+++ src/main/kotlin/com/github/xalvarez/springformmailer/mailgun/MailgunConfiguration.kt
@@ -4,28 +4,26 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 import javax.validation.constraints.Email
-import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 
 @Configuration
-@ConfigurationProperties(prefix = "mailgun")
+@ConfigurationProperties(prefix = MAILGUN_CONFIGURATION_PREFIX)
 class MailgunConfiguration {
 
     @Email
     @Value("\${mailgun.email.sender}")
     lateinit var sender: String
 
-    @NotEmpty
-    lateinit var domain: String
+    @NotBlank
+    lateinit var baseUrl: String
 
     @NotNull
     @Value("\${mailgun.email.recipients}")
     lateinit var recipients: Array<String>
 
-    @NotEmpty
+    @NotBlank
     lateinit var apiKey: String
-
-    fun getMailgunEndpointUrl(): String {
-        return if (domain.contains("localhost")) domain else "https://api.mailgun.net/v3/$domain/messages"
-    }
 }
+
+private const val MAILGUN_CONFIGURATION_PREFIX = "mailgun"
